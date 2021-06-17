14be866350ef47fc838321faf3c3a06d100cf67e
mercy-ikuma <kumayme23@gmail.com>
2021-06-17 02:18:11 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main is the entry point
- *Return 0 is a succes
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *main is entry point
- *return 0(success)
+ *main - Entry point
+ *return: Always 0 (success)
  */
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+   printf("with proper grammar, but the outcome is a piece of art,\n");
+   return (0);
 }
   
