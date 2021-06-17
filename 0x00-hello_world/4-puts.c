3eb4a716d9878391ce80671f3940b6ba5c63bca6
mercy-ikuma <kumayme23@gmail.com>
2021-06-17 01:50:29 -0700

    c hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main is the entry point
+ *Return 0 is a succes
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
