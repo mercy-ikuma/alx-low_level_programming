13d15ff38548b21af0b18dd8971fcafa3cfefa8f
mercy-ikuma <kumayme23@gmail.com>
2021-06-17 02:24:34 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  *main - Entry point
- *return: Always 0 (success)
+ *Return: Always 0 (success)
  */
 
 int main(void)
 {
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-   return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
   
