d4a9bc3b565ae64b7b5c3d26b27a445e1dc9cf91
mercy-ikuma <84702006+mercy-ikuma@users.noreply.github.com>
2021-06-24 19:48:07 +0300

    loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,22 +1,27 @@
 #include "holberton.h"
-
 /**
- * print_diagonal - print character "\" n times
- * @n: number of characters to draw
- */
-
+* print_diagonal - draws a diagonal line
+* @n: takes in an integer
+*/
 void print_diagonal(int n)
 {
-	int draw;
+	int i, j;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
 	{
-		for (draw = 1; draw <= n; draw++)
+		for (j = 0; j <= i; j++)
 		{
-			_putchar('\\');
-			_putchar('\n');
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
 		}
 	}
-	_putchar('\n');
-
 }
