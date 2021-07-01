ed1b2b1d2120bb0efee44e81c34aef98ff1d7fae
mercy-ikuma <84702006+mercy-ikuma@users.noreply.github.com>
2021-07-01 21:01:51 +0300

    pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,23 +1,20 @@
 #include "holberton.h"
 /**
- * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
  *
- * Return: s1[i] - s2[i]
+ * Return: void
  */
-int _strcmp(char *s1, char *s2)
+void reverse_array(int *a, int n)
 {
 	int i;
+	int temp;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (i = 0; i < n--; i++)
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 	}
-	return (0);
 }
