065439712f3fde3700ce8a6dad5dee30ced7d180
mercy-ikuma <84702006+mercy-ikuma@users.noreply.github.com>
2021-10-15 18:15:30 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int main(void)
 {
 int n;
 srand(time(0));
-n = rand() -RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
@@ -23,7 +23,7 @@ printf("%i is zero\n", n);
 }
 else
 {
-  printf("%i is negative\n", n);
+printf("%i is negative\n", n);
 }
 return (0);
 }
